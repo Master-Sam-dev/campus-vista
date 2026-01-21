053aa9e7bd24c11268079e6157f61859b3bfd0b3
Sam <mastersamdev@gmail.com>
2026-01-21 13:58:11 +0500

    Fix GitHub Pages config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:"/campus-vista",
-  resolve: {
-    dedupe: ["three"]
-  }
+  base: '/campus-vista/'
 })
